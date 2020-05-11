c4236f77065d9f269ac2d5561ab5bed65789c5fb
lautschrift <steffen@lautschrift.com>
2020-05-11 19:21:46 +0200

    Update ContentVitrine.php

--- src/Resources/contao/classes/ContentVitrine.php
+++ src/Resources/contao/classes/ContentVitrine.php
@@ -3,11 +3,26 @@ class ContentVitrine extends ContentElement
 {
 	protected $strTemplate = 'ce_vitrine';
 
+
+	public function generate()
+			{
+					if (TL_MODE == 'BE') {
+							$template = new \BackendTemplate('be_wildcard');
+							$template->wildcard = '### '.utf8_strtoupper($GLOBALS['TL_LANG']['CTE']['vitrine'][0]).' ###';
+
+							return $template->parse();
+					}
+
+					return parent::generate();
+			}
+
+
 	protected function compile()
 	{
-		$rs = Database::getInstance()
+		$rs = \Database::getInstance()
 		->query('SELECT * FROM tl_content');
 
-		$this->Template->vitrine = $rs->fetchAllAssoc();
+		$this->Template->Vitrine = $rs->fetchAllAssoc();
 	}
+
 }
