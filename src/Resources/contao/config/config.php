8a71dfc4d804879b9727b0e62b6e8228626d0de4
lautschrift <steffen@lautschrift.com>
2020-05-11 19:34:51 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,19 +3,17 @@
 /**
  * Back end modules
  */
-array_insert($GLOBALS['BE_MOD']['content'], 1, array
-(
-	'Vitrine' => array
-	(
-		'tables'	=> array('tl_vitrine'),
 
-	)
-));
 
 /**
  * Content elements
  */
 
-$GLOBALS['TL_CTE']['Vitrine'] = array(
-		'vitrine' => 'ContentVitrine'
-);
+
+array_insert($GLOBALS['TL_CTE']['Vitrine'] ,1, array(
+		'Vitrine' => 'Lautschrift\\VitrineBundle\\Resources\\contao\\classes\\ContentVitrine'
+));
+
+
+//$GLOBALS['TL_CTE']['Museum']['Museum'] = '\\lautschrift\\museum-bundle\\src\\Resources\\contao\\classes\\ContentMuseum';
+$GLOBALS['TL_LANG']['CTE']['Vitrine'] = array('Vitrine','Einbinden einer Museums Vitrine ins Fundfenster');
