2784a81d5a6df616158704403d54a2be789cad3d
lautschrift <steffen@lautschrift.com>
2020-05-11 20:54:29 +0200

    wip

--- src/_DependencyInjection/VitrineExtension.php
+++ src/_DependencyInjection/VitrineExtension.php
@@ -3,10 +3,10 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
-class MuseumExtension extends ConfigurableExtension
+class VitrineExtension extends ConfigurableExtension
 {
 	protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
 	{
-		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config')); 
+		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 		$loader->load(’services.yml’);
 	} }
