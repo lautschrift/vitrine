83caef2f89fd29f39d00f3fd6c1cf9ea1305a43f
lautschrift <steffen@lautschrift.com>
2020-05-12 15:32:04 +0200

    Update VitrineExtension.php

--- src/DependencyInjection/VitrineExtension.php
+++ src/DependencyInjection/VitrineExtension.php
@@ -7,6 +7,6 @@ class MuseumExtension extends ConfigurableExtension
 {
 	protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
 	{
-		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config')); 
+		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 		$loader->load(’services.yml’);
 	} }
