778bdd9a170671df6b192c59f49044f5332565bf
lautschrift <steffen@lautschrift.com>
2020-05-12 09:03:22 +0200

    ..

---
+++ src/VitrineBundle.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types = 1);
+namespace Lautschrift\VitrineBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * This is the bundle class. */
+class VitrineBundle extends Bundle
+{ }
